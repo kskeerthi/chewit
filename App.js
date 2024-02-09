e03c5a12716d49f08ee07d98afce6d9578c7adbb
kskeetu <sk.keetu@gmail.com>
2024-02-09 16:38:57 +0530

    changes

--- App.js
+++ App.js
@@ -1,3 +1,4 @@
 const heading = React.createElement("h1", {id:'heading'}, "Hello keerthi learn React please!!");
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(heading);
+\
