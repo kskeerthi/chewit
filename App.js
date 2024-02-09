ed2c382142faa5d36964de735e43fae0874f3397
Ashutosh Kumar Singh <ashutoshkumarsingh@Ashutoshs-MacBook-Air.local>
2024-02-09 13:35:18 +0530

    Episode1-CDNReact

---
+++ App.js
@@ -0,0 +1,3 @@
+const heading = React.createElement("h1", {id:'heading'}, "Hello keerthi learn React please!!");
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(heading);
